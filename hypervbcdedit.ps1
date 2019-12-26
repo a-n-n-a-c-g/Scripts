a55d5fb906707794a533665162f028eea77605cb
Anna <annacguthrie5@gmail.com>
2019-12-26 14:46:47 -0700

    auto restart after hypverbcdedit change

--- hypervbcdedit.ps1
+++ hypervbcdedit.ps1
@@ -3,10 +3,12 @@ if ($confirmation -eq 'y') {
     if (($($(bcdedit | select-string hypervisorlaunchtype) -split '\s+')[1]) -eq "Off") {
         Write-Host "Enabling Hyper-V"
         bcdedit /set hypervisorlaunchtype auto
+        Restart-Computer
     }
     else {
         Write-Host "Disabling Hyper-V"
         bcdedit /set hypervisorlaunchtype off
+        Restart-Computer
     }
 }
 else {
